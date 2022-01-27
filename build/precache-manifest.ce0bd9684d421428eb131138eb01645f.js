f401ca1ca8f691ab3f5adb2dc406aa12dfe9e583
tamim08cse <tamimhasan08@gmail.com>
2022-01-28 00:16:02 +0600

    initial commit

---
+++ build/precache-manifest.ce0bd9684d421428eb131138eb01645f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a552f3aa9e715a270714ac8b0ed4090",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61070f24948a95c29870",
+    "url": "/static/css/main.78cdb1b6.chunk.css"
+  },
+  {
+    "revision": "7ae87f3f01c5c37aff5c",
+    "url": "/static/js/2.0b7c5e8b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0b7c5e8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61070f24948a95c29870",
+    "url": "/static/js/main.43ae9f8c.chunk.js"
+  },
+  {
+    "revision": "f2349edc127edabb2fc6",
+    "url": "/static/js/runtime-main.197ca643.js"
+  },
+  {
+    "revision": "4c17826e1261b79e8a3a1202c0987ec8",
+    "url": "/static/media/background-landing.4c17826e.jpg"
+  },
+  {
+    "revision": "52ec66aa73668ee82efd75ab2f085459",
+    "url": "/static/media/edit.52ec66aa.svg"
+  },
+  {
+    "revision": "b1060c5b0ff9dad3f12630787bf72f54",
+    "url": "/static/media/logo-app.b1060c5b.svg"
+  },
+  {
+    "revision": "0ed185aee6e1b43d6e03b75b3a8af000",
+    "url": "/static/media/save.0ed185ae.svg"
+  },
+  {
+    "revision": "3d4b2818898d9983d090486a605e3a72",
+    "url": "/static/media/thumbs-up.3d4b2818.svg"
+  }
+]);
